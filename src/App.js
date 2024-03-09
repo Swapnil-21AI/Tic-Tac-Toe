691581c97d29757ce3fce371736ea957fcb9f923
Swapnil Kuyate <104415301+Swapnil-21AI@users.noreply.github.com>
2024-03-09 05:08:50 +0000

    d

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
+import TicTacToe from './components/tic-tac-toe/tic-tac-toe'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <TicTacToe></TicTacToe>
     </div>
   );
 }
